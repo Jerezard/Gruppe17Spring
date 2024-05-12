bd180e955136df4a5627df942f021c7cfe240d09
Jerezard <jeremys-1999@hotmail.com>
2024-05-12 03:57:21 +0200

    car rental entity

--- src/main/java/com/example/demo/carRental/CarRental.java
+++ src/main/java/com/example/demo/carRental/CarRental.java
@@ -3,6 +3,9 @@ package com.example.demo.carRental;
 import jakarta.persistence.*;
 import java.sql.Date;
 
+import com.example.demo.car.Car;
+import com.example.demo.user.User;
+
 @Entity
 public class CarRental {
 
@@ -21,8 +24,70 @@ public class CarRental {
     private Status rentalStatus;
     
 
-
-    private int carID;
-    private int userID;
+    @OneToOne
+    private Car car;
+   
+    @OneToOne
+    private User user;
     
+    
+    public CarRental(){
+
+    }
+
+    private CarRental(Date rentalStarDate, Date rentalEnDate, Status rentalStatus){
+
+        this.rentalStartDate = rentalStarDate;
+        this.rentalEndDate = rentalEndDate;
+        this.rentalStatus = rentalStatus;
+
+    }
+
+    public int getRentalID() {
+        return rentalID;
+    }
+
+    public void setRentalID(int rentalID) {
+        this.rentalID = rentalID;
+    }
+
+    public Date getRentalStartDate() {
+        return rentalStartDate;
+    }
+
+    public void setRentalStartDate(Date rentalStartDate) {
+        this.rentalStartDate = rentalStartDate;
+    }
+
+    public Date getRentalEndDate() {
+        return rentalEndDate;
+    }
+
+    public void setRentalEndDate(Date rentalEndDate) {
+        this.rentalEndDate = rentalEndDate;
+    }
+
+    public Status getRentalStatus() {
+        return rentalStatus;
+    }
+
+    public void setRentalStatus(Status rentalStatus) {
+        this.rentalStatus = rentalStatus;
+    }
+
+    public Car getCar() {
+        return car;
+    }
+
+    public void setCar(Car car) {
+        this.car = car;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
 }
