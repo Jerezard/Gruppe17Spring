524c42abc1d87e04e0aea9c84d94f863959a7f4e
Jerezard <jeremys-1999@hotmail.com>
2024-05-20 21:00:41 +0200

    New repo and methods in car repo

--- src/main/java/com/example/demo/car/CarRepository.java
+++ src/main/java/com/example/demo/car/CarRepository.java
@@ -1,9 +1,13 @@
 package com.example.demo.car;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface CarRepository extends JpaRepository<Car, Integer> {
-
+    Optional<Car> findByRegistrationNumber(String registrationNumber);
+    List<Car> findByAvailabilityStatus(boolean availabilityStatus);
 }
