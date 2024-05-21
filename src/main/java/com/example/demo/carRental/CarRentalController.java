46f20ec0944ac93bd82abc914d80c1ce9a29f3f1
Jerezard <jeremys-1999@hotmail.com>
2024-05-21 22:19:49 +0200

    Exception handling when creating car rental

--- src/main/java/com/example/demo/carRental/CarRentalController.java
+++ src/main/java/com/example/demo/carRental/CarRentalController.java
@@ -2,12 +2,16 @@ package com.example.demo.carRental;
 
 import java.util.List;
 
+import org.apache.catalina.connector.Response;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -18,8 +22,22 @@ public class CarRentalController {
     private CarRentalService carRentalService;
 
     @PostMapping
-    public ResponseEntity<CarRental> createCarRental(@RequestBody CarRentalDto carRentalDto){
-        CarRental carRental = carRentalService.createCarRental(carRentalDto);
+    public ResponseEntity<?> createCarRental(@RequestBody CarRentalDto carRentalDto){
+        if(carRentalDto.getCar() == null || carRentalDto.getUser() == null ){
+            return ResponseEntity.badRequest().body("Car and User must be provided");
+        }
+        
+        try{
+            CarRental carRental = carRentalService.createCarRental(carRentalDto);
+            return ResponseEntity.ok(carRental);
+        } catch (Exception e){
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("An error occurred: " + e.getMessage());
+        }
+    }
+
+    @PostMapping("/{id}/status")
+    public ResponseEntity<CarRental> updateCarRentalStatus(@PathVariable int id,@RequestParam String status){
+        CarRental carRental = carRentalService.updateCarRentalStatus(id, status);
         return ResponseEntity.ok(carRental);
     }
 
