1dbdc7bb26c39a57a81e36a73c832338dfc3a9da
Jerezard <jeremys-1999@hotmail.com>
2024-05-21 22:18:04 +0200

    Method to find CarRentals by carid, userid and status from repo

--- src/main/java/com/example/demo/carRental/CarRentalRepository.java
+++ src/main/java/com/example/demo/carRental/CarRentalRepository.java
@@ -1,9 +1,11 @@
 package com.example.demo.carRental;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface CarRentalRepository extends JpaRepository<CarRental, Integer> {
-    
+    Optional<CarRental> findByCar_CarIDAndUser_UserIDAndRentalStatus(int carId, int userId, CarRental.Status status);
 }
