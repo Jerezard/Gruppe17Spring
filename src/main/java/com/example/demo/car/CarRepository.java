d0b638425616b16a85eeb9be55de8988e2640877
Jerezard <jeremys-1999@hotmail.com>
2024-05-21 22:21:58 +0200

    Query to find cars ordered by price from repo

--- src/main/java/com/example/demo/car/CarRepository.java
+++ src/main/java/com/example/demo/car/CarRepository.java
@@ -4,10 +4,17 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface CarRepository extends JpaRepository<Car, Integer> {
     Optional<Car> findByRegistrationNumber(String registrationNumber);
     List<Car> findByAvailabilityStatus(boolean availabilityStatus);
+    
+    @Query("SELECT c FROM Car c JOIN c.prices p GROUP BY c ORDER BY MIN(p.amount) ASC")
+    List<Car> findAllCarsOrderedByLowestPrice();
+
+    @Query("SELECT c FROM Car c JOIN c.prices p GROUP BY c ORDER BY MAX(p.amount) DESC")
+    List<Car> findAllCarsOrderedByHighestPrice();
 }
