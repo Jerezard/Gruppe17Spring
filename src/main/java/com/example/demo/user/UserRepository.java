3ffb6e1cc17e60bb92539b0ddc6017608b606ec3
Jerezard <jeremys-1999@hotmail.com>
2024-05-21 22:13:25 +0200

    Methods to get user by email and phone number from repository

--- src/main/java/com/example/demo/user/UserRepository.java
+++ src/main/java/com/example/demo/user/UserRepository.java
@@ -3,5 +3,6 @@ package com.example.demo.user;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, Integer> {
-    
+    boolean existsByEmail(String email);
+    boolean existsByPhoneNumber(String phoneNumber);
 }
