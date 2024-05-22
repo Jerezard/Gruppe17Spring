23df690905ee744ef86763ef66c39bd815fa6079
Jerezard <jeremys-1999@hotmail.com>
2024-05-23 01:08:48 +0200

    removed unused method

--- src/main/java/com/example/demo/Repository/CarRentalRepository.java
+++ src/main/java/com/example/demo/Repository/CarRentalRepository.java
@@ -1,5 +1,6 @@
 package com.example.demo.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -9,6 +10,5 @@ import com.example.demo.Entity.CarRental;
 
 @Repository
 public interface CarRentalRepository extends JpaRepository<CarRental, Integer> {
-    Optional<CarRental> findByCar_CarIDAndUser_UserIDAndRentalStatus(int carId, int userId, CarRental.Status status);
     List<CarRental> findByCarCarID(int carId);
 }
